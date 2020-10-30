bd497e0769114874f58a0d1e41b5e649c4de68d0
Marek <marek.sawick@gmail.com>
2020-10-30 12:06:02 +0100

    App component modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import {BreakpointObserver, Breakpoints, MediaMatcher} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,12 @@ import {MediaMatcher} from '@angular/cdk/layout';
 export class AppComponent {
   title = 'KaM';
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
 
