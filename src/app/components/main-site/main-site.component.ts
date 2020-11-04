23ddd49845c826fb7c5658abc1cdc5c980821b85
Hubert <roginski.h@gmail.com>
2020-11-04 02:07:23 +0100

    add animations to main-site and second parallax

--- src/app/components/main-site/main-site.component.ts
+++ src/app/components/main-site/main-site.component.ts
@@ -1,22 +1,56 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  Renderer2, ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-main-site',
   templateUrl: './main-site.component.html',
-  styleUrls: ['./main-site.component.css']
+  styleUrls: ['./main-site.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainSiteComponent implements OnInit {
 
-  public scrollHeight: ElementRef;
+  public activeImage = 'https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_6.png?raw=true';
+  public layer5Flags = '../../../assets/parallax/layer5Flags-1.png';
+  public layer6Flags;
+  public layersOfFood;
 
-
-  constructor() { }
+  constructor(private cdRef: ChangeDetectorRef) {
+  }
 
   ngOnInit(): void {
+    let i = 1;
+
+    setInterval(() => {
+      this.layer5Flags = `../../../assets/parallax/layer5Flags-${i}.png`;
+      this.layer6Flags = `../../../assets/parallax/layer6Flags-${i}.png`;
+      this.layersOfFood = `../../../assets/parallax/parallax2-layer-${i}.png`;
+      this.cdRef.detectChanges();
+      if (i === 10) {
+        i = 1;
+      } else {
+        i++;
+      }
+    }, 100);
   }
 
-  public onScroll(): void{
-    const scrollHeight = this.scrollHeight.nativeElement as HTMLElement;
-    console.log(' dupa ' + scrollHeight);
+
+  @HostListener('window:scroll', ['$event']) onWindowScroll(e): void {
+    console.log(e.target.scrollingElement.scrollTop);
+
+    // Your Code Here
+
   }
+
+  public onContentScroll(e: Event): void {
+    console.log('parallax' + (e.target as Element).scrollTop);
+  }
+
+
 }
