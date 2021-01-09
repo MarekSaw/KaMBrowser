b3fd4252538e612d196b48868cc75d52d5efc9be
Marek <marek.sawick@gmail.com>
2021-01-09 13:22:17 +0100

    Building menu modified

--- src/app/components/game/gui-components/village/city/components/building-menu/building-menu.component.ts
+++ src/app/components/game/gui-components/village/city/components/building-menu/building-menu.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Building {
+  imgSource: string;
+  imgAlt: string;
+  resourceInfo: string;
+}
+
 @Component({
   selector: 'app-building-menu',
   templateUrl: './building-menu.component.html',
   styleUrls: ['./building-menu.component.css']
 })
 export class BuildingMenuComponent implements OnInit {
+
+  buildings: Building[] = [
+    {imgSource: 'assets/buildings/stable.bmp', imgAlt: 'Stable', resourceInfo: 'Planks: 6 Stone: 10  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/mill.bmp', imgAlt: 'Mill', resourceInfo: 'Planks: 3 Stone: 2  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'}
+  ];
 
   constructor() { }
 
