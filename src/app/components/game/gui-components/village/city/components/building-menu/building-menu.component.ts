53d50a1943d5c9f8f0ff3c2fffb83497219230b2
Marek <marek.sawick@gmail.com>
2021-01-09 19:16:58 +0100

    Building menu modified - Tile's imgId added

--- src/app/components/game/gui-components/village/city/components/building-menu/building-menu.component.ts
+++ src/app/components/game/gui-components/village/city/components/building-menu/building-menu.component.ts
@@ -14,13 +14,9 @@ export interface Building {
 export class BuildingMenuComponent implements OnInit {
 
   buildings: Building[] = [
-    {imgSource: 'assets/buildings/stable.bmp', imgAlt: 'Stable', resourceInfo: 'Planks: 6 Stone: 10  Time: 30 seconds'},
-    {imgSource: 'assets/buildings/mill.bmp', imgAlt: 'Mill', resourceInfo: 'Planks: 3 Stone: 2  Time: 30 seconds'},
-    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
-    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
-    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
-    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'},
-    {imgSource: 'assets/buildings/bakery.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'}
+    {imgSource: 'assets/buildings/8.bmp', imgAlt: 'Stable', resourceInfo: 'Planks: 6 Stone: 10  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/9.bmp', imgAlt: 'Mill', resourceInfo: 'Planks: 3 Stone: 2  Time: 30 seconds'},
+    {imgSource: 'assets/buildings/19.bmp', imgAlt: 'Bakery', resourceInfo: 'Planks: 4 Stone: 6  Time: 30 seconds'}
   ];
 
   constructor() { }
