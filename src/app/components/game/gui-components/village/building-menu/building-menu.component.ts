cb01bf84b9261602bcc2cb1be8d421df069059c8
Marek <marek.sawick@gmail.com>
2021-01-15 18:08:42 +0100

    Building and Upgrade menu fixed

--- src/app/components/game/gui-components/village/building-menu/building-menu.component.ts
+++ src/app/components/game/gui-components/village/building-menu/building-menu.component.ts
@@ -3,6 +3,7 @@ import {FieldServiceService} from '../../../../../services/field-service.service
 import {BuildingService} from '../../../../../services/building.service';
 import {FieldModel} from '../../../../../models/FieldModel';
 import {BuildingModel} from '../../../../../models/BuildingModel';
+import {Router} from '@angular/router';
 
 
 
@@ -16,7 +17,7 @@ export class BuildingMenuComponent implements OnInit {
   field: FieldModel;
   building: BuildingModel;
 
-  constructor(private buildService: BuildingService, private fieldService: FieldServiceService) {
+  constructor(private buildService: BuildingService, private fieldService: FieldServiceService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -26,12 +27,18 @@ export class BuildingMenuComponent implements OnInit {
 
   buildBuilding(): void {
     this.field.buildingLevel++;
+    this.field.className = `build-${this.field.className}`;
     this.fieldService.updateField(this.field).subscribe();
+    this.redirect();
   }
 
   private findBuilding(map: string, fieldNumber: number): void {
     this.buildService.findBuildingByMapAndFieldNumber(map, fieldNumber).subscribe(value => {
       this.building = value;
     });
+  }
+
+  private redirect(): void {
+    this.router.navigate(['/game/village/upgrade-menu']);
   }
 }
