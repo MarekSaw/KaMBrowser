1b3527a2dd664baaf8cc8d595cbccfc5cba4ed8c
Marek <marek.sawick@gmail.com>
2021-01-26 20:05:32 +0100

    PageLoading control added

--- src/main/app/app.component.ts
+++ src/main/app/app.component.ts
@@ -1,7 +1,9 @@
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
-import {BreakpointObserver, Breakpoints, MediaMatcher} from '@angular/cdk/layout';
+import {Component} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent} from '@angular/router';
+import {ObservableService} from './services/observable.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,8 @@ import {map, shareReplay} from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'KaM';
+  public loadingBarEnabled = true;
+  public zIndexValue: number;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -17,6 +21,33 @@ export class AppComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              private observer: ObservableService,
+              private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.navigationEvents(event);
+    });
+  }
+
+  private navigationEvents(event: RouterEvent): void {
+    console.log(this.loadingBarEnabled);
+    if (event instanceof NavigationStart) {
+      this.zIndexValue = 10;
+      this.loadingBarEnabled = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.zIndexValue = -10;
+      this.loadingBarEnabled = false;
+    }
+    if (event instanceof NavigationCancel) {
+      this.zIndexValue = -10;
+      this.loadingBarEnabled = false;
+    }
+    if (event instanceof NavigationError) {
+      this.zIndexValue = -10;
+      this.loadingBarEnabled = false;
+    }
+  }
+
 }
 
